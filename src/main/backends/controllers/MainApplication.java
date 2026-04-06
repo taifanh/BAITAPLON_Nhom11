76a1524cdf9c694ded299fc682f1dc902e12d64b
taianhuet <25021642@vnu.edu.vn>
2026-04-06 22:28:27 +0700

    change database

--- src/main/backends/controllers/MainApplication.java
+++ src/main/backends/controllers/MainApplication.java
@@ -12,7 +12,7 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
 //        Application.setUserAgentStylesheet(Application.STYLESHEET_MODENA);
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/org/example/views/signin.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
         stage.setTitle("sign in!");
         stage.setScene(scene);
         stage.show();
